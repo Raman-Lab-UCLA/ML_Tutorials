76fbd746faf45f6a01f7797da7934ad989350d8c
cyyeung1234 <36492956+cyyeung1234@users.noreply.github.com>
2022-05-13 13:32:44 -0700

    Add files via upload

---
+++ ANN for Multilayer Thin Films/ANN_Test.py
@@ -0,0 +1,32 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Dec 19 12:18:22 2019
+
+@author: cyyeu
+"""
+from keras.models import Sequential
+from keras.layers import Dense, Flatten
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from keras.models import load_model
+
+#load model
+model = load_model('model.h5')
+
+#Index data and generate prediction
+i = 10
+x = np.arange(0.5, 10.1, 0.1).reshape(1,96)
+outputANN = model.predict(input_test[i].reshape(1,4))[0][:]
+outputExcelTest = output_test[i].reshape(1,96)[0][:]
+
+#Plot outputANN (prediction) and outputExcelTest (validation data)
+fig, ax = plt.subplots()
+ax.set(xlabel = 'Wavelength (μm)', ylabel = 'Absorption (A.U.)')
+ax.plot(x[0], outputExcelTest, label = 'ground truth', linewidth = 4.0)
+ax.plot(x[0], outputANN, label = 'predictions', linewidth = 4.0)
+plt.xlim(0.5,10)
+plt.ylim(0,1)
+plt.legend()
+plt.show()
