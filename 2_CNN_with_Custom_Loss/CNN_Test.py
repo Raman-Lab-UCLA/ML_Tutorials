4f3c66be3895b076c346444acfd667377383a4f1
cyyeung1234 <36492956+cyyeung1234@users.noreply.github.com>
2022-06-02 11:17:02 -0700

    Update CNN_Test.py

--- 2_CNN_with_Custom_Loss/CNN_Test.py
+++ 2_CNN_with_Custom_Loss/CNN_Test.py
@@ -12,11 +12,9 @@ model = load_model('/.../model.h5', compile = False)
 # load img
 def loadImages(path):
     loadedImages = []
-    # return array of images
     filesname = glob.glob(path)
     filesname.sort()
     for imgdata in filesname:
-        # determine whether it is an image.
         if os.path.isfile(os.path.splitext(os.path.join(path, imgdata))[0] + ".png"): 
             img_array = cv2.imread(os.path.join(path, imgdata)) 
             img_array = np.float32(img_array)
@@ -26,26 +24,21 @@ def loadImages(path):
 
 # path = local path of images
 path = '/.../Images_test/*.png'
-imgs = loadImages(path) # loaded images
+imgs = loadImages(path)
 imgs = np.array(imgs)
-#imgs = np.squeeze(CNN_input_test)
 
 for i in range(len(imgs)): 
-    
     y = np.array(pd.read_csv('/.../cross_length_test.csv', header = 0, index_col=0))
-    #y = CNN_output_test
     x = np.arange(4, 12, 0.01).reshape([1,800])
-    predictions_test1 = model.predict(imgs[i].reshape(-1,128,128,1))
+    predictions_test1 = model.predict(imgs[i].reshape(-1,imgs.shape[1],imgs.shape[1],1))
     print(predictions_test1)
-    
     fig, ax = plt.subplots()
     ax.set(xlabel = 'Wavelength (µm)', ylabel = 'Absorption')
     ax.plot(x[0], y[i].reshape(1,800)[0][:], label = 'Ground Truth', linewidth = 4.0)    
     ax.plot(x[0], predictions_test1.reshape(1,800)[0][:], label = 'Predictions', linewidth = 4.0)  
-    
     plt.rcParams['font.size'] = '18'
     plt.xlim(4,12)
     plt.ylim(0,1)
-    #plt.legend()
-    plt.savefig('/.../test_results/'+str(i)+'.png', bbox_inches="tight")
+    plt.legend()
+    plt.savefig('/.../Test_Results/'+str(i)+'.png', bbox_inches="tight")
     plt.show()
